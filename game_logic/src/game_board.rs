1f28fbc145ee1374eb1ac3be272e7700c8134602
Aliaksandr Vasilenka <Aliaksandr_Vasilenka@epam.com>
2024-04-15 11:04:55 +0200

    Implemented all win and draw checks

--- game_logic/src/game_board.rs
+++ game_logic/src/game_board.rs
@@ -63,27 +63,74 @@ impl GameBoard {
     }
 
     pub fn check_for_result(&self, player: Player) -> Option<GameResult> {
-        self.check_rows(player)
+        self.check_rows(player)?;
 
-        //self.check_columns(player)?;
+        self.check_columns(player)?;
 
-        //self.check_diagonals(player)?;
+        self.check_diagonals(player)?;
 
-        //self.check_for_draw()?
+        self.check_for_draw()
     }
 
     fn check_rows(&self, player: Player) -> Option<GameResult> {
-        let row_check = |c: &Cell| -> bool {
-            if let Cell::Occupied(p) = c {
-                *p == player
-            } else {
-                false
-            }
-        };
+        let row_checker = |c: &Cell| -> bool { *c == Cell::Occupied(player) };
 
         self.cells
             .iter()
-            .any(|r| r.iter().all(row_check))
+            .any(|r| r.iter().all(row_checker))
             .then_some(GameResult::Win(player))
+    }
+
+    fn check_columns(&self, player: Player) -> Option<GameResult> {
+        let cell_checker = |cell: &Cell| -> bool { *cell == Cell::Occupied(player) };
+
+        let column_checker = |row: &[Cell; BOARD_SIZE], col: usize| -> bool {
+            row.get(col).is_some_and(cell_checker)
+        };
+
+        let column_indices = 0..BOARD_SIZE;
+        column_indices
+            .into_iter()
+            .any(|col| self.cells.iter().all(|r| column_checker(r, col)))
+            .then_some(GameResult::Win(player))
+    }
+
+    fn check_diagonals(&self, player: Player) -> Option<GameResult> {
+        let cell_checker = |cell: &Cell| -> bool { *cell == Cell::Occupied(player) };
+
+        let mut cell_in_diag_indx: usize = 0;
+
+        // Checking from top-left to bottom-right diagonal
+        let result = self
+            .cells
+            .iter()
+            .all(|r| {
+                let check_result = r.get(cell_in_diag_indx).is_some_and(cell_checker);
+                cell_in_diag_indx += 1;
+                check_result
+            })
+            .then_some(GameResult::Win(player));
+
+        if result.is_some() {
+            return result;
+        }
+
+        // Checking from top-rigth to bottom-left diagonal
+        cell_in_diag_indx = 2;
+        self.cells
+            .iter()
+            .all(|r| {
+                let check_result = r.get(cell_in_diag_indx).is_some_and(cell_checker);
+                cell_in_diag_indx -= 1;
+                check_result
+            })
+            .then_some(GameResult::Win(player))
+    }
+
+    fn check_for_draw(&self) -> Option<GameResult> {
+        self.cells
+            .iter()
+            .all(|r| r.iter().all(|c| *c != Cell::Empty))
+            .then_some(GameResult::Draw)
     }
 }
